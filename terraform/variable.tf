de84397644339dcaf9627864ca7b0eeb39fa3ae7
baraqheart <baraqheart@baraqheart.baraqheartVm>
2024-04-06 20:01:18 +0100

    [skip ci]

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,10 +1,7 @@
 variable "location" {
-  default = "westeurope"
+  default = "West Europe"
 }
 
-variable "sp" {
-  default = ["d651569c-06a4-43f4-8af3-5cf5f2e48312","uJ58Q~V7wSofsHUlq6-_v8vPA-InVO.H9i.5IcLT"]
-}
 
 variable "network" {
   default = ["10.5.0.0/16", "10.5.1.0/24", "10.5.2.0/24"]
@@ -23,7 +20,7 @@ variable "dns_prefix" {
 }
 
 variable "node_pool" {
-  default = ["default",1,"Standard_D2s_v3"]
+  default = ["default",1,"Standard_D2_v2"]
 }
 
 variable "network_profile" {
